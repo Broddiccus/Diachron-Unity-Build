3757b6b7be6bf8180c3fb9357bb5283c3acb330d
Brody Mulligan <Brody_man02@hotmail.com>
2018-02-07 19:49:25 -0500

    Barebones attack
    Bare Bones of attack structures, heavy attack slows player, light attack performs a lunge, no attack happens yet.

--- Diachron.Alpha/Assets/Scripts/Attack.cs
+++ Diachron.Alpha/Assets/Scripts/Attack.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Attack : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+public class Attack : MonoBehaviour 
+{	
+	public bool ata;
+	public bool lunge;
+	public int[] checker;
+	public bool[] checkerCheck;
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey (KeyCode.Z) == true) {
+			ata = true;
+		} else {
+			ata = false;
+		}
+		if (Input.GetKeyDown (KeyCode.X) == true && checkerCheck [0] == true) {
+			lunge = true;
+			checkerCheck [0] = false;
+		}
+		if (checkerCheck [0] == false) {
+			checker [0]++;
+		}
+		if (checker [0] > 20) {
+			checkerCheck [0] = true;
+			checker [0] = 0;
+		}
 	}
 }
--- Diachron.Alpha/Assets/Scripts/Movement.cs
+++ Diachron.Alpha/Assets/Scripts/Movement.cs
@@ -2,10 +2,26 @@
 
 public class Movement : MonoBehaviour
 {
+	public Attack ata;
+	public float speed;
+	public int lungeCheck;
 	void Update()
 	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+		if (ata.ata == true) {
+			speed = 1;
+		} else {
+			speed = 3;
+		}
+		if (ata.lunge == true) {
+			speed = 6;
+			lungeCheck++;
+			if (lungeCheck > 4) {
+				ata.lunge = false;
+				lungeCheck = 0;
+			}
+		}
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
 		transform.Translate(x, 0, 0);
 		transform.Translate(0, 0, z);
